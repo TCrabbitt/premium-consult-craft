e43950f9003463e6de9b4ecd8904f5e4e1ee974b
Hiten Singh <hitenxsingh@gmail.com>
2025-04-29 13:39:52 +0530

    ...

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     >
       <div className="container mx-auto px-4 md:px-6 flex items-center justify-between">
         <Link to="/" className="flex items-center">
-          <img src="/Hallmark Workforce Solutions LOGO - black text -.png" alt="Hallmark" className="h-10 md:h-12" />
+          <img src="/hallmark.png" alt="Hallmark" className="h-10 md:h-12" />
         </Link>
 
         {/* Desktop Navigation */}
